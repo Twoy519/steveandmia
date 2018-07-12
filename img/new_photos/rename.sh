0a84f2bef15b23513b59ad4d4b7917139ad2af4e
Porter <troy.porter1@gmail.com>
2018-07-12 22:55:43 +0100

    Adding new photos!

--- img/new_photos/rename.sh
+++
@@ -1,2 +0,0 @@
-# for i in *; do mv "$i" "$(echo $i|sed 's/ /_/g')"; done
-for i in *; do mv "$i" "$(echo $i|tr A-Z a-z)"; done
--- img/resize.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for i in group_*/*; do
-  convert "$i" -resize 700x500 -background none -gravity center -extent 700x500 "$i"
-done
